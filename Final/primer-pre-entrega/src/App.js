86fe2e842352fec9db5f347a2df37be996b70dae
Fermín Martínez <feermartinez1504@gmail.com>
2022-10-18 12:57:15 -0300

    Commit

--- Final/primer-pre-entrega/src/App.js
+++ Final/primer-pre-entrega/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={"Bienvenido a LA CANCHA, aquí encontrarás todo para tu look!"}/>
-      <Footer/>
     </div>
   );
 }
--- Final/primer-pre-entrega/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <h6>Todos los derechos reservados.</h6>
-    </footer>
-  );
-};
-
-export default Footer;
